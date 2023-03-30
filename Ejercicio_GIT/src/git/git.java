e6068fb26ffb3bc810e20200e7c414083c08efdd
Alejandro <adelfresnomartin@gmail.com>
2023-03-30 09:40:38 +0200

    nuevo Alumno

--- Ejercicio_GIT/src/git/git.java
+++ Ejercicio_GIT/src/git/git.java
@@ -4,5 +4,6 @@ public class git {
 public static void main(String[] args) {
 	System.out.println("Listado de alumnos 1º DAW");
 	System.out.println("alumno 1:");
+	Alumno aa=new Alumno("pepe","perez","32423424Y");
 }
 }
